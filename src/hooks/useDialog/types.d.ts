e03e0cf827a16dd63ad47dfc74aa0d0162ebf45e
Eugenio <eugenio.ciaglia@gmail.com>
2022-06-24 20:02:04 +0200

    table multiselect

---
+++ src/hooks/useDialog/types.d.ts
@@ -0,0 +1,34 @@
+
+interface IOpenModalPayload {
+    selectedId?: string;
+    itemData?: object;
+}
+
+interface IUseDialogAction {
+    type: UseDialogActions;
+    payload?: IOpenModalPayload;
+}
+
+interface IUseDialogState {
+    id: string;
+    opened: boolean;
+    selectedId?: string;
+    itemData?: any;
+}
+
+type UseDialogResult = {
+    open: () => void;
+    close: () => void;
+    setSelectedId: (selectedId: string) => void;
+    setItemData: (itemData: any) => void;
+    opened: boolean;
+    selectedId?: string;
+    itemData?: any
+}
+
+const enum UseDialogActions {
+    SET_ITEM_ID = 'SET_ITEM_ID',
+    SET_ITEM_DATA = 'SET_ITEM_DATA',
+    OPEN_MODAL = 'OPEN_MODAL',
+    CLOSE_MODAL = 'CLOSE_MODAL',
+}
